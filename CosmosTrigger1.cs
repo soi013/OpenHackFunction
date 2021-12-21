92a7a1f25c9b4c00507a7abdad0b27f1c15e450c
SUZUKI Kensuke <suzuki.kensuke.16@gmail.com>
2021-12-21 16:11:46 +0900

    initial commit

---
+++ CosmosTrigger1.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace Contoso.Function
+{
+    public static class CosmosTrigger1
+    {
+        [FunctionName("CosmosTrigger1")]
+        public static void Run([CosmosDBTrigger(
+            databaseName: "contoso-movies",
+            collectionName: "MergedOrders",
+            ConnectionStringSetting = "contosodb_DOCUMENTDB",
+            LeaseCollectionName = "leases")]IReadOnlyList<Document> input,
+            ILogger log)
+        {
+            if (input != null && input.Count > 0)
+            {
+                log.LogInformation("Documents modified " + input.Count);
+                log.LogInformation("First document Id " + input[0].Id);
+            }
+        }
+    }
+}
