b9bd5dd57c6ef129f51f9c5ae7b6ee8f368dc13a
soi013 <soi2flatline@gmail.com>
2021-12-22 14:16:22 +0900

    映画タイトルを取得する機能を追加

--- CosmosTrigger1.cs
+++ CosmosTrigger1.cs
@@ -13,6 +13,7 @@ namespace Contoso.Function
     {
         private const string outputCollection = "PopularMovies";
         private const string inputCollection = "MergedOrders";
+        private const string subInputCollection = "Item";
         private const string targetDataBase = "contoso-movies";
         private const string endpointUrl = "https://contoso-db.documents.azure.com:443/";
         private const string authorizationKey = "tGkmlAsfIe6XPu8ZIU9Z76j75sT6VU5gBYhypIZWMqYwJMjAJT8UMGXgn169oIo0bcGws0jUg0FDae389Mwdrg==";
@@ -52,52 +53,26 @@ namespace Contoso.Function
         private static async Task QueryItems(ILogger log, IAsyncCollector<MovieRankItem> myDestinationCollection)
         {
             //MergedOrdersの中から、人気順でTop10のProduct IDを取得するクエリ
-            var sqlQueryText1 = @"
+            var sqlQueryWork = @"
 SELECT count(1) AS ProductIdcount,c.ProductId
 FROM MergedOrders m
 JOIN c IN m.Details
 GROUP BY c.ProductId";
 
-            //10個のProductIDからItemsの結果を取得してランクを付けるクエリ
-            var sqlQueryText2 = "SELECT * FROM c WHERE c.LastName = 'Andersen'";
-
-            Console.WriteLine("Running query: {0}\n", sqlQueryText1);
+            Console.WriteLine("Running query: {0}\n", sqlQueryWork);
 
             CosmosClient inputCosmosClient = new CosmosClient(endpointUrl, authorizationKey);
             var inputContainer = inputCosmosClient.GetContainer(targetDataBase, inputCollection);
-
-            QueryDefinition queryDefinition = new QueryDefinition(sqlQueryText1);
-            var feedIterator = inputContainer.GetItemQueryIterator<WorkItem>(queryDefinition);
-
-            var workList = new List<WorkItem>();
-
-            while (feedIterator.HasMoreResults)
-            {
-                FeedResponse<WorkItem> response = await feedIterator.ReadNextAsync();
-                foreach (var item in response)
-                {
-                    log.LogInformation($"items = {item}");
-                    workList.Add(item);
-                    //なぜか書き込めない。
-                    // var responseUpsert = await workContainer.UpsertItemAsync(item, new Microsoft.Azure.Cosmos.PartitionKey(item.ProductId));
-                    // log.LogInformation($"response = {responseUpsert}");
-                }
-            }
+            List<WorkItem> workList = await GetItemsFromContainer<WorkItem>(log, sqlQueryWork, inputContainer);
 
             var guid = Guid.NewGuid();
             var calcTime = DateTime.UtcNow;
-            var topMovies = workList
+            var subInputContainer = inputCosmosClient.GetContainer(targetDataBase, subInputCollection);
+
+            var topMovies = await Task.WhenAll(workList
                 .OrderByDescending(x => x.ProductIdcount)
                 .Take(10)
-                .Select((x, i) => new MovieRankItem
-                (
-                    ProductId: x.ProductId,
-                    ProductIdcount: x.ProductIdcount,
-                    MovieTitle: "HOGE",
-                    Rank: i + 1,
-                    Guid: guid,
-                    CalcTime: calcTime
-                ));
+                .Select(async (x, i) => await CreateMovieRank(x, i, guid, calcTime, log, subInputContainer)));
 
             foreach (var item in topMovies)
             {
@@ -105,8 +80,48 @@ GROUP BY c.ProductId";
                 await myDestinationCollection.AddAsync(item);
             }
         }
+
+        private static async Task<List<T>> GetItemsFromContainer<T>(ILogger log, string queryText, Container sourceContainer)
+        {
+            QueryDefinition queryDefinition = new QueryDefinition(queryText);
+            var feedIterator = sourceContainer.GetItemQueryIterator<T>(queryDefinition);
+
+            var workList = new List<T>();
+
+            while (feedIterator.HasMoreResults)
+            {
+                FeedResponse<T> response = await feedIterator.ReadNextAsync();
+                foreach (var item in response)
+                {
+                    log.LogInformation($"items = {item}");
+                    workList.Add(item);
+                }
+            }
+
+            return workList;
+        }
+
+        private static async Task<MovieRankItem> CreateMovieRank(WorkItem x, int i, Guid guid, DateTime calcTime, ILogger log, Container inputContainer)
+        {
+            //10個のProductIDからItemsの結果を取得してランクを付けるクエリ
+            var sqlQueryText2 = @$"SELECT TOP 1 * From c WHERE c.ItemId = {x.ProductId}";
+
+            var movies = await GetItemsFromContainer<MovieItem>(log, sqlQueryText2, inputContainer);
+            var title = movies.FirstOrDefault()?.ProductName ?? "Unknown Title";
+
+            return new MovieRankItem
+                    (
+                        ProductId: x.ProductId,
+                        ProductIdcount: x.ProductIdcount,
+                        MovieTitle: title,
+                        Rank: i + 1,
+                        Guid: guid,
+                        CalcTime: calcTime
+                    );
+        }
     }
 
     public record WorkItem(string ProductId, int ProductIdcount);
+    public record MovieItem(string ItemId, string ProductName, string CategoryId, string Category);
     public record MovieRankItem(string ProductId, int ProductIdcount, string MovieTitle, int Rank, Guid Guid, DateTime CalcTime);
 }
